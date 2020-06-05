b699bd8758e34186e6a105cf1ad4428ff637b31d
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-06-05 17:05:06 +0200

    changement arraylist en array dans le tp3 et ajout premiere class tp de lundi

---
+++ src/fr/algorithmie/AffichageInverse.java
@@ -0,0 +1,23 @@
+package fr.algorithmie;
+
+public class AffichageInverse {
+
+	public static void main(String[] args) {
+
+		int[] array = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+
+		System.out.println("\n");
+
+		for (int i = array.length - 1; i >= 0; i--) {
+			System.out.print(array[i] + " ");
+		}
+
+		System.out.println("\n");
+
+	}
+
+}
--- src/fr/boucles/ExerciceTableauEntier.java
+++ src/fr/boucles/ExerciceTableauEntier.java
@@ -4,23 +4,34 @@ import java.util.ArrayList;
 
 /**
  * Classe exercice sur les tableaux
+ * 
  * @author robin
  *
  */
 public class ExerciceTableauEntier {
 
 	public static void main(String[] args) {
-		
+
 		ArrayList<Integer> tab = new ArrayList<Integer>(10);
-		for(int i = 1; i < 11; i++) {
+		for (int i = 1; i < 11; i++) {
 			tab.add(i);
 		}
-		
+
 		System.out.println(tab.get(0));
 		System.out.println(tab.size());
-		System.out.println(tab.get(tab.size()-1));
+		System.out.println(tab.get(tab.size() - 1));
 		tab.set(4, 8);
-		
-		System.out.println(tab);
+
+		System.out.println(tab+"\n");
+
+		// version 2 avec array tout court
+		int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		System.out.println(array[0]);
+		System.out.println(array.length);
+		System.out.println(array[array.length-1]);
+		array[4] = 8;
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
 	}
 }
