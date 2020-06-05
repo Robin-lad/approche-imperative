bbcdd45d247591ec8da96461df544deef41eb28a
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-06-05 16:44:11 +0200

    changement des accolades

--- src/fr/boucles/ExerciceBouclesEtTests.java
+++ src/fr/boucles/ExerciceBouclesEtTests.java
@@ -25,30 +25,34 @@ public class ExerciceBouclesEtTests {
 		System.out.println("\n");
 
 		for (int i = 0; i < a.length; i++) {
-			if (a[i] > 3)
+			if (a[i] > 3) {
 				System.out.print(a[i] + " ");
+			}
 		}
 
 		System.out.println("\n");
 
 		// valeur modulo 2 = 0 donc pair
 		for (int i = 0; i < a.length; i++) {
-			if (a[i] % 2 == 0)
+			if (a[i] % 2 == 0) {
 				System.out.print(a[i] + " ");
+			}
 		}
 
 		System.out.println("\n");
 		// i (donc index) modulo 2 = 0 donc index pair
 		for (int i = 0; i < a.length; i++) {
-			if (i % 2 == 0)
+			if (i % 2 == 0) {
 				System.out.print(a[i] + " ");
+			}
 		}
 
 		System.out.println("\n");
 		// valeur de i modulo 2 = 1 donc impair
 		for (int i = 0; i < a.length; i++) {
-			if (Math.abs(a[i] % 2) == 1)
+			if (Math.abs(a[i] % 2) == 1) {
 				System.out.print(a[i] + " ");
+			}
 		}
 	}
 }
